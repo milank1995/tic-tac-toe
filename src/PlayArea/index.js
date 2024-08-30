77017f5c80fc873b374399f541ed569409136f0c
Milan Kodinariya <milankodinariya1995@gmail.com>
2024-08-30 23:55:18 +0530

    Setup in the game logic

--- src/PlayArea/index.js
+++ src/PlayArea/index.js
@@ -1,19 +1,65 @@
-import React from "react"
+import React, {useEffect} from "react"
 
+let isYourTurn = false
+
+const winningCombo = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
 
 export const PlayArea = props => {
+    const indexArray = [...new Array(9)].map((_, i) => ({
+        value: '',
+        index: i + 1
+    }))
+    const [boxes, setBoxes] = React.useState(indexArray)
+
+    useEffect(() => {
+        isWinner()
+    }, [boxes]);
+
+    const onBoxClick = (index) => {
+        if (isYourTurn) {
+            boxes[index].value = "X"
+            isYourTurn = false
+        } else {
+            boxes[index].value = "O"
+            isYourTurn = true
+        }
+        setBoxes([...boxes])
+    }
+
+    const isWinner = () => {debugger
+        winningCombo.forEach(winning => {
+            const comb1 = boxes[winning[0] - 1].value
+            const comb2 = boxes[winning[1] - 1].value
+            const comb3 = boxes[winning[2] - 1].value
+            if (comb1 && comb2 && comb3 && comb1 === comb2 && comb2 === comb3) {
+                console.log('winner')
+            }
+        })
+    }
+
     return (
         <div className="container">
             <div className="boxes">
-                <button id="0"></button>
-                <button id="1"></button>
-                <button id="2"></button>
-                <button id="3"></button>
-                <button id="4"></button>
-                <button id="5"></button>
-                <button id="6"></button>
-                <button id="7"></button>
-                <button id="8"></button>
+                {
+                    boxes.map((_, index) => (
+                        <button
+                            key={index.toString()}
+                            id={index}
+                            onClick={() => onBoxClick(index)}
+                        >
+                            {_.value}
+                        </button>
+                    ))
+                }
             </div>
         </div>
     )
