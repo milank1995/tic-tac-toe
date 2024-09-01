4f3a724366891fad2eae6b7a1516183ef5281006
Milan Kodinariya <milankodinariya1995@gmail.com>
2024-09-01 11:59:45 +0530

    Implemented play with computer features

---
+++ src/PlayWithComputer/index.js
@@ -0,0 +1,129 @@
+import React, {useEffect, useState} from "react"
+import Confetti from 'react-confetti'
+
+const winningCombo = [
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 6, 9],
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [1, 5, 9],
+    [3, 5, 7]
+];
+
+export const PlayArea = props => {
+    const indexArray = [...new Array(9)].map((_, i) => ({
+        value: '',
+        index: i + 1
+    }))
+    const [boxes, setBoxes] = useState(indexArray)
+    // const [isYourTurn, setIsYourTurn] = useState(false)
+    let isYourTurn = true
+    const [isWinner, setIsWinner] = useState(false)
+
+    useEffect(() => {
+        onWinnerChecking()
+    }, [boxes]);
+
+    const onBoxClick = (index) => {debugger
+        if (isYourTurn) {
+            boxes[index].value = "X"
+            isYourTurn = false
+        } else {
+            boxes[index].value = "O"
+            isYourTurn = true
+        }
+        setBoxes([...boxes])
+        if(!isWinner && !isYourTurn) {
+            setTimeout(() => onBoxClick(onAutoPlaying(boxes)), 1000)
+        }
+    }
+
+    const onWinnerChecking = () => {
+        winningCombo.forEach(winning => {
+            const comb1 = boxes[winning[0] - 1].value
+            const comb2 = boxes[winning[1] - 1].value
+            const comb3 = boxes[winning[2] - 1].value
+            if (comb1 && comb2 && comb3 && comb1 === comb2 && comb2 === comb3) {
+                setIsWinner(true)
+                document.getElementsByTagName("body")[0].style.overflow = "hidden"
+            }
+        })
+    }
+
+    const onNewGame = () => {
+        isYourTurn = false
+        setIsWinner(false)
+        setBoxes([...indexArray])
+        document.getElementsByTagName("body")[0].style.overflow = "auto"
+    }
+
+    const onAutoPlaying = (updatedBoxes) => {
+        const remainingBoxes = updatedBoxes.filter(box => !box.value)
+        const randomNumber = Math.floor(Math.random() * remainingBoxes.length)
+        const index = remainingBoxes[randomNumber].index
+        return updatedBoxes.findIndex(box => box.index === index)
+    }
+
+    return (
+        <main>
+
+            {
+                isWinner ? (
+                    <Confetti
+                        recycle={false}
+                        numberOfPieces={1000}
+                    />
+                ) : null
+            }
+
+            <h1 className="game-title">Tic Tac Toe</h1>
+            <div className="container">
+                <div className="game">
+                    {
+                        boxes.map((_, index) => (
+                            <button
+                                className="box"
+                                key={index.toString()}
+                                id={index.toString()}
+                                onClick={() => onBoxClick(index)}
+                                disabled={_.value || isWinner}
+                            >
+                                {_.value}
+                            </button>
+                        ))
+                    }
+                </div>
+            </div>
+            <div className="msg-container">
+                {
+                    isWinner ? (
+                        <>
+                            <p id="msg">Winner</p>
+                            <button
+                                id="new-btn"
+                                onClick={onNewGame}
+                            >
+                                New Game
+                            </button>
+                        </>
+                    ) : (
+                        <>
+                            <h3 className="turn-msg">
+                                {isYourTurn ? "X" : "O"} Your turn...
+                            </h3>
+                            <button
+                                id="reset-btn"
+                                onClick={onNewGame}
+                            >
+                                Reset Game
+                            </button>
+                        </>
+                    )
+                }
+
+            </div>
+        </main>
+    )
+}
